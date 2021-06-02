41277bbb96b4d8029e8013bd6dbb7457dedf4cdf
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2021-06-02 09:21:33 +0100

    style terms page

--- src/pages/Terms/Terms.jsx
+++ src/pages/Terms/Terms.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import { tl8 } from '../../utils/locale';
+
+import './Terms.scss';
 
 const Terms = () => {
   return (
-    <div className="container">
-      <h2>
-        <strong>Terms and Conditions</strong>
-      </h2>
+    <div className="container terms-page">
+      <h2 className="text-center page-title">{tl8('terms.page_title')}</h2>
 
-      <p>Welcome to MHA!</p>
+      <p className="body">Welcome to MHA!</p>
 
-      <p>
+      <p className="body">
         These terms and conditions outline the rules and regulations for the use
         of MHA's Website, located at mental-health-assistance.netlify.app.
       </p>
 
-      <p>
+      <p className="body">
         By accessing this website we assume you accept these terms and
         conditions. Do not continue to use MHA if you do not agree to take all
         of the terms and conditions stated on this page.
       </p>
 
-      <p>
+      <p className="body">
         The following terminology applies to these Terms and Conditions, Privacy
         Statement and Disclaimer Notice and all Agreements: "Client", "You" and
         "Your" refers to you, the person log on this website and compliant to
@@ -37,16 +38,17 @@ const Terms = () => {
         referring to same.
       </p>
 
-      <h3>
+      <br />
+      <h3 className="heading">
         <strong>Cookies</strong>
       </h3>
 
-      <p>
+      <p className="body">
         We employ the use of cookies. By accessing MHA, you agreed to use
         cookies in agreement with the MHA's Privacy Policy.{' '}
       </p>
 
-      <p>
+      <p className="body">
         Most interactive websites use cookies to let us retrieve the user’s
         details for each visit. Cookies are used by our website to enable the
         functionality of certain areas to make it easier for people visiting our
@@ -54,39 +56,27 @@ const Terms = () => {
         cookies.
       </p>
 
-      <h3>
+      <br />
+      <h3 className="heading">
         <strong>License</strong>
       </h3>
 
-      <p>
+      <p className="body">
         Unless otherwise stated, MHA and/or its licensors own the intellectual
         property rights for all material on MHA. All intellectual property
         rights are reserved. You may access this from MHA for your own personal
         use subjected to restrictions set in these terms and conditions.
       </p>
 
-      <p>You must not:</p>
-      <ul>
-        <li>Republish material from MHA</li>
-        <li>Sell, rent or sub-license material from MHA</li>
-        <li>Reproduce, duplicate or copy material from MHA</li>
-        <li>Redistribute content from MHA</li>
+      <p className="body">You must not:</p>
+      <ul className="body">
+        <li>- Republish material from MHA</li>
+        <li>- Sell, rent or sub-license material from MHA</li>
+        <li>- Reproduce, duplicate or copy material from MHA</li>
+        <li>- Redistribute content from MHA</li>
       </ul>
 
-      <p>
-        This Agreement shall begin on the date hereof. Our Terms and Conditions
-        were created with the help of the{' '}
-        <a href="https://www.termsandconditionsgenerator.com">
-          Terms And Conditions Generator
-        </a>{' '}
-        and the{' '}
-        <a href="https://www.generateprivacypolicy.com">
-          Privacy Policy Generator
-        </a>
-        .
-      </p>
-
-      <p>
+      <p className="body">
         Parts of this website offer an opportunity for users to post and
         exchange opinions and information in certain areas of the website. MHA
         does not filter, edit, publish or review Comments prior to their
@@ -99,66 +89,68 @@ const Terms = () => {
         on this website.
       </p>
 
-      <p>
+      <p className="body">
         MHA reserves the right to monitor all Comments and to remove any
         Comments which can be considered inappropriate, offensive or causes
         breach of these Terms and Conditions.
       </p>
 
-      <p>You warrant and represent that:</p>
+      <p className="body">You warrant and represent that:</p>
 
-      <ul>
+      <ul className="body">
         <li>
-          You are entitled to post the Comments on our website and have all
+          - You are entitled to post the Comments on our website and have all
           necessary licenses and consents to do so;
         </li>
         <li>
-          The Comments do not invade any intellectual property right, including
-          without limitation copyright, patent or trademark of any third party;
+          - The Comments do not invade any intellectual property right,
+          including without limitation copyright, patent or trademark of any
+          third party;
         </li>
         <li>
-          The Comments do not contain any defamatory, libelous, offensive,
+          - The Comments do not contain any defamatory, libelous, offensive,
           indecent or otherwise unlawful material which is an invasion of
           privacy
         </li>
         <li>
-          The Comments will not be used to solicit or promote business or custom
-          or present commercial activities or unlawful activity.
+          - The Comments will not be used to solicit or promote business or
+          custom or present commercial activities or unlawful activity.
         </li>
       </ul>
 
-      <p>
+      <p className="body">
         You hereby grant MHA a non-exclusive license to use, reproduce, edit and
         authorize others to use, reproduce and edit any of your Comments in any
         and all forms, formats or media.
       </p>
 
-      <h3>
+      <br />
+      <h3 className="heading">
         <strong>Hyperlinking to our Content</strong>
       </h3>
 
-      <p>
+      <p className="body">
         The following organizations may link to our Website without prior
         written approval:
       </p>
 
-      <ul>
-        <li>Government agencies;</li>
-        <li>Search engines;</li>
-        <li>News organizations;</li>
+      <ul className="body">
+        <li>- Government agencies;</li>
+        <li>- Search engines;</li>
+        <li>- News organizations;</li>
         <li>
-          Online directory distributors may link to our Website in the same
+          - Online directory distributors may link to our Website in the same
           manner as they hyperlink to the Websites of other listed businesses;
           and
         </li>
         <li>
-          System wide Accredited Businesses except soliciting non-profit
+          - System wide Accredited Businesses except soliciting non-profit
           organizations, charity shopping malls, and charity fundraising groups
           which may not hyperlink to our Web site.
         </li>
       </ul>
 
-      <p>
+      <p className="body">
         These organizations may link to our home page, to publications or to
         other Website information so long as the link: (a) is not in any way
         deceptive; (b) does not falsely imply sponsorship, endorsement or
@@ -166,22 +158,22 @@ const Terms = () => {
         fits within the context of the linking party’s site.
       </p>
 
-      <p>
+      <p className="body">
         We may consider and approve other link requests from the following types
         of organizations:
       </p>
 
-      <ul>
-        <li>commonly-known consumer and/or business information sources;</li>
-        <li>dot.com community sites;</li>
-        <li>associations or other groups representing charities;</li>
-        <li>online directory distributors;</li>
-        <li>internet portals;</li>
-        <li>accounting, law and consulting firms; and</li>
-        <li>educational institutions and trade associations.</li>
+      <ul className="body">
+        <li>- commonly-known consumer and/or business information sources;</li>
+        <li>- dot.com community sites;</li>
+        <li>- associations or other groups representing charities;</li>
+        <li>- online directory distributors;</li>
+        <li>- internet portals;</li>
+        <li>- accounting, law and consulting firms; and</li>
+        <li>- educational institutions and trade associations.</li>
       </ul>
 
-      <p>
+      <p className="body">
         We will approve link requests from these organizations if we decide
         that: (a) the link would not make us look unfavorably to ourselves or to
         our accredited businesses; (b) the organization does not have any
@@ -190,14 +182,14 @@ const Terms = () => {
         context of general resource information.
       </p>
 
-      <p>
+      <p className="body">
         These organizations may link to our home page so long as the link: (a)
         is not in any way deceptive; (b) does not falsely imply sponsorship,
         endorsement or approval of the linking party and its products or
         services; and (c) fits within the context of the linking party’s site.
       </p>
 
-      <p>
+      <p className="body">
         If you are one of the organizations listed in paragraph 2 above and are
         interested in linking to our website, you must inform us by sending an
         e-mail to MHA. Please include your name, your organization name, contact
@@ -206,38 +198,42 @@ const Terms = () => {
         site to which you would like to link. Wait 2-3 weeks for a response.
       </p>
 
-      <p>Approved organizations may hyperlink to our Website as follows:</p>
+      <p className="body">
+        Approved organizations may hyperlink to our Website as follows:
+      </p>
 
-      <ul>
-        <li>By use of our corporate name; or</li>
-        <li>By use of the uniform resource locator being linked to; or</li>
+      <ul className="body">
+        <li>- By use of our corporate name; or</li>
+        <li>- By use of the uniform resource locator being linked to; or</li>
         <li>
-          By use of any other description of our Website being linked to that
+          - By use of any other description of our Website being linked to that
           makes sense within the context and format of content on the linking
           party’s site.
         </li>
       </ul>
 
-      <p>
+      <p className="body">
         No use of MHA's logo or other artwork will be allowed for linking absent
         a trademark license agreement.
       </p>
 
-      <h3>
+      <br />
+      <h3 className="heading">
         <strong>iFrames</strong>
       </h3>
 
-      <p>
+      <p className="body">
         Without prior approval and written permission, you may not create frames
         around our Webpages that alter in any way the visual presentation or
         appearance of our Website.
       </p>
 
-      <h3>
+      <br />
+      <h3 className="heading">
         <strong>Content Liability</strong>
       </h3>
 
-      <p>
+      <p className="body">
         We shall not be hold responsible for any content that appears on your
         Website. You agree to protect and defend us against all claims that is
         rising on your Website. No link(s) should appear on any Website that may
@@ -246,17 +242,18 @@ const Terms = () => {
         any third party rights.
       </p>
 
-      <h3>
+      <br />
+      <h3 className="heading">
         <strong>Your Privacy</strong>
       </h3>
 
-      <p>Please read Privacy Policy</p>
-
-      <h3>
+      <p className="body">Please read Privacy Policy</p>
+      <br />
+      <h3 className="heading">
         <strong>Reservation of Rights</strong>
       </h3>
 
-      <p>
+      <p className="body">
         We reserve the right to request that you remove all links or any
         particular link to our Website. You approve to immediately remove all
         links to our Website upon request. We also reserve the right to amen
@@ -265,53 +262,55 @@ const Terms = () => {
         these linking terms and conditions.
       </p>
 
-      <h3>
+      <br />
+      <h3 className="heading">
         <strong>Removal of links from our website</strong>
       </h3>
 
-      <p>
+      <p className="body">
         If you find any link on our Website that is offensive for any reason,
         you are free to contact and inform us any moment. We will consider
         requests to remove links but we are not obligated to or so or to respond
         to you directly.
       </p>
 
-      <p>
+      <p className="body">
         We do not ensure that the information on this website is correct, we do
         not warrant its completeness or accuracy; nor do we promise to ensure
         that the website remains available or that the material on the website
         is kept up to date.
       </p>
 
-      <h3>
+      <br />
+      <h3 className="heading">
         <strong>Disclaimer</strong>
       </h3>
 
-      <p>
+      <p className="body">
         To the maximum extent permitted by applicable law, we exclude all
         representations, warranties and conditions relating to our website and
         the use of this website. Nothing in this disclaimer will:
       </p>
 
-      <ul>
+      <ul className="body">
         <li>
-          limit or exclude our or your liability for death or personal injury;
+          - limit or exclude our or your liability for death or personal injury;
         </li>
         <li>
-          limit or exclude our or your liability for fraud or fraudulent
+          - limit or exclude our or your liability for fraud or fraudulent
           misrepresentation;
         </li>
         <li>
-          limit any of our or your liabilities in any way that is not permitted
-          under applicable law; or
+          - limit any of our or your liabilities in any way that is not
+          permitted under applicable law; or
         </li>
         <li>
-          exclude any of our or your liabilities that may not be excluded under
-          applicable law.
+          - exclude any of our or your liabilities that may not be excluded
+          under applicable law.
         </li>
       </ul>
 
-      <p>
+      <p className="body">
         The limitations and prohibitions of liability set in this Section and
         elsewhere in this disclaimer: (a) are subject to the preceding
         paragraph; and (b) govern all liabilities arising under the disclaimer,
@@ -319,7 +318,7 @@ const Terms = () => {
         statutory duty.
       </p>
 
-      <p>
+      <p className="body">
         As long as the website and the information and services on the website
         are provided free of charge, we will not be liable for any loss or
         damage of any nature.
